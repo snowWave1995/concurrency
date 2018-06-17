85d9eab61ea054a79e56ab0230c4637db3603c3d
snowWave666 <2450756004@qq.com>
2018-06-17 18:17:33 +0800

    init project

---
+++ src/test/java/com/snowwave/concurrency/MapExample.java
@@ -0,0 +1,44 @@
+package com.snowwave.concurrency;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created by zhangfuqiang on 2018/6/17.
+ */
+@Slf4j
+public class MapExample {
+    private static Map<Integer,Integer> map = new HashMap<>();
+
+    private static int threadNum = 1;
+    private static int clientNum = 5000;
+
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadNum);
+        for (int i = 0; i < clientNum; i++) {
+            final int threadNum = i;
+            exec.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    func(threadNum);
+                    semaphore.release();
+                }catch (Exception e) {
+                    log.error("exception",e);
+                }
+            });
+        }
+        exec.shutdown();
+        log.info("count:{}",map.size());
+    }
+
+    public static void func(int threadNum) {
+        map.put(threadNum,threadNum);
+    }
+}
