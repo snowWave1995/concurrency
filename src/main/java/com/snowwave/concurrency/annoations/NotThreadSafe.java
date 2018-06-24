d1d98fdd94aae4f7acb493d1ca3c0462b69674a3
snowWave666 <2450756004@qq.com>
2018-06-24 14:14:34 +0800

    add annoations

---
+++ src/main/java/com/snowwave/concurrency/annoations/NotThreadSafe.java
@@ -0,0 +1,17 @@
+package com.snowwave.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by zhangfuqiang on 2018/6/24.
+ * 用来标记线程不安全的类
+ */
+@Target(ElementType.TYPE) //该注解可以加在什么地方，如类，方法等等
+@Retention(RetentionPolicy.SOURCE)//该注解的保存策略
+public @interface NotThreadSafe {
+
+    String value() default "";
+}
