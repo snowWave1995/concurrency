cfcc0d5e0540a19471cf47b3c62a4ea24de92280
snowWave666 <2450756004@qq.com>
2018-08-05 20:34:20 +0800

    jedisTest

--- src/main/java/com/snowwave/concurrency/example/cache/RedisClient.java
+++ src/main/java/com/snowwave/concurrency/example/cache/RedisClient.java
@@ -1,6 +1,7 @@
 package com.snowwave.concurrency.example.cache;
 
 import org.springframework.stereotype.Component;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 import javax.annotation.Resource;
@@ -14,4 +15,27 @@ public class RedisClient {
     @Resource(name = "redisPool")
     private JedisPool jedisPool;
 
+    public void set(String key,String value) throws Exception {
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            jedis.set(key,value);
+        } finally {
+            if (null != jedis) {
+                jedis.close();
+            }
+        }
+    }
+
+    public String get(String key) throws Exception {
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            return jedis.get(key);
+        } finally {
+            if (null != jedis) {
+                jedis.close();
+            }
+        }
+    }
 }
