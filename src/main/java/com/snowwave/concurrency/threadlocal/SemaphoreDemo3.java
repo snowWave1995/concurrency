fa31a89b353b708e70b5d8871f5aedc83d6ac6f8
snowWave1995 <2450756004@qq.com>
2018-11-25 19:11:04 +0800

    3个并发组件

---
+++ src/main/java/com/snowwave/concurrency/threadlocal/SemaphoreDemo3.java
@@ -0,0 +1,33 @@
+package com.snowwave.concurrency.threadlocal;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created by zhangfuqiang on 2018/11/25.
+ * 同时只能有10个线程保存数据
+ */
+public class SemaphoreDemo3 {
+    private static final int THREAD_COUNT = 30;
+    private static ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_COUNT);
+    private static Semaphore s = new Semaphore(10);
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < THREAD_COUNT; i++) {
+
+            threadPool.execute(() -> {
+                    try {
+                        s.acquire();
+                        System.out.println("save data");
+                        s.release();
+                    } catch (InterruptedException e) {
+
+                    }
+            });
+        }
+        threadPool.shutdown();
+
+    }
+}
